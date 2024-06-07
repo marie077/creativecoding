4c6c11be980dcc29b97b53197d13837df91b08c6
marie <marieow35@gmail.com>
2024-06-07 13:21:56 -0400

    noise

--- Generative Visuals/KPOP/June14/sketch.js
+++ Generative Visuals/KPOP/June14/sketch.js
@@ -1,7 +1,25 @@
 function setup() {
     createCanvas(400, 400);
+    pixelDensity(1);
   }
   
   function draw() {
     background(220);
+    loadPixels();
+    var yoff = 0.0;
+    for (var y = 0; y < height; y++) {
+      var xoff = 0.0;
+      for (var x = 0; x < width; x++) {
+        var index = (x + y * width) * 4;
+        var r = noise(xoff, yoff) * 255;
+        pixels[index + 0] = r;
+        pixels[index + 1] = r;
+        pixels[index + 2] = r;
+        pixels[index + 3] = 255;
+        xoff += 0.01;
+      }
+      yoff += 0.01;
+    }
+    updatePixels();
+    
   }
