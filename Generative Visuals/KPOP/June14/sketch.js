0b79a9e22c86d3409fc16963d9369521b91c7720
marie <marieow35@gmail.com>
2024-06-07 16:49:01 -0400

    vectors

--- Generative Visuals/KPOP/June14/sketch.js
+++ Generative Visuals/KPOP/June14/sketch.js
@@ -1,25 +1,35 @@
+var scl = 20;
+var cols, rows;
+var inc = 0.05;
+var fr;
+
 function setup() {
     createCanvas(400, 400);
     pixelDensity(1);
+    cols = floor(width/scl);
+    rows = floor(height/scl);
+    fr = createP('');
   }
   
   function draw() {
     background(220);
-    loadPixels();
     var yoff = 0.0;
-    for (var y = 0; y < height; y++) {
+    for (var y = 0; y < rows; y++) {
       var xoff = 0.0;
-      for (var x = 0; x < width; x++) {
-        var index = (x + y * width) * 4;
-        var r = noise(xoff, yoff) * 255;
-        pixels[index + 0] = r;
-        pixels[index + 1] = r;
-        pixels[index + 2] = r;
-        pixels[index + 3] = 255;
-        xoff += 0.01;
+      for (var x = 0; x < cols; x++) {
+        var angle = noise(xoff, yoff) * TWO_PI;
+        var v = p5.Vector.fromAngle(angle);
+        xoff += inc;
+        stroke(0);
+        push();
+        translate(x * scl, y * scl);
+        rotate(v.heading());
+        line(0, 0, scl, 0);
+
+        pop();
+        // rect(x * scl, y * scl, scl, scl);
       }
-      yoff += 0.01;
+      yoff += inc;
     }
-    updatePixels();
-    
+    fr.html(floor(frameRate()));
   }
